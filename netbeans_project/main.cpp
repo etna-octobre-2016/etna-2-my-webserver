26c3e80860543002ad09d4d716819545f87ab9b0
Said AHEMT <said.ahemt@nolaroads.com>
2015-02-06 15:20:25 +0100

    organizes the main.cpp file

--- netbeans_project/main.cpp
+++ netbeans_project/main.cpp
@@ -5,24 +5,22 @@
  * Created on 2 février 2015, 22:38
  */
 
+// Dependencies
 #include <cstdlib>
 #include <iostream>
 
+// MySQL includes
 #include <mysql_connection.h>
-
+#include <mysql_driver.h>
 #include <cppconn/driver.h>
 #include <cppconn/exception.h>
 #include <cppconn/resultset.h>
 #include <cppconn/statement.h>
 
-#include "mysql_driver.h"
-
-using namespace std;
-
-int main(void)
+void testMySQL()
 {
-    cout << endl;
-    cout << "Running 'SELECT * FROM users'" << endl;
+    std::cout << std::endl;
+    std::cout << "Running 'SELECT * FROM users'" << std::endl;
 
     try
     {
@@ -43,14 +41,14 @@ int main(void)
       
       while (res->next())
       {
-        cout << "\t... MySQL replies: ";
+        std::cout << "\t... MySQL replies: ";
         
         /* Access column data by alias or column name */
-        cout << res->getString("name") << endl;
-        cout << "\t... MySQL says it again: ";
+        std::cout << res->getString("name") << std::endl;
+        std::cout << "\t... MySQL says it again: ";
         
         /* Access column fata by numeric offset, 1 is the first column */
-        cout << res->getString(1) << endl;
+        std::cout << res->getString(1) << std::endl;
       }
       delete res;
       delete stmt;
@@ -58,13 +56,23 @@ int main(void)
     }
     catch (sql::SQLException &e)
     {
-      cout << "# ERR: SQLException in " << __FILE__;
-      cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << endl;
-      cout << "# ERR: " << e.what();
-      cout << " (MySQL error code: " << e.getErrorCode();
-      cout << ", SQLState: " << e.getSQLState() << " )" << endl;
+      std::cout << "# ERR: SQLException in " << __FILE__;
+      std::cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << std::endl;
+      std::cout << "# ERR: " << e.what();
+      std::cout << " (MySQL error code: " << e.getErrorCode();
+      std::cout << ", SQLState: " << e.getSQLState() << " )" << std::endl;
     }
-    cout << endl;
-    return EXIT_SUCCESS;
+    std::cout << std::endl;
 }
 
+void testSQLite()
+{
+    
+}
+
+int main(void)
+{
+    testMySQL();
+    //testPostgreSQL();
+    return 0;
+}
